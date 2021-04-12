b46d08716475da646c8fdefe16ddd0261e603119
DevSP <contatodevsp@gmail.com>
2021-04-12 11:14:23 -0300

    Configurando - font, chakra e colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,6 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home</title>
-      </Head>
-
       <h1>Hello World</h1>
     </>
   )
---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'custom.bg',
+        color: 'custom.highlight',
+      }
+    }
+  },
+  fonts: {
+    body: 'Poppins',
+    heading: 'Poppins',
+  },
+  colors: {
+    custom: {
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'bg': '#F5F8FA',
+      'highlight': '#FFBA08',
+      'text': '#F5F8FA',
+      'info': '#DADADA',
+      'text_dark': '#47585B',
+      'info_dark': '#999999',
+    }
+  }
+})
