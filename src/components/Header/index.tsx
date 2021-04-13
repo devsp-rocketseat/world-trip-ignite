37bad15a1a3b9683ee47975c272ea34c386251c7
DevSP <contatodevsp@gmail.com>
2021-04-13 20:03:39 -0300

    Responsividade: Component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 interface Props {
   btnBack?: boolean
@@ -7,24 +8,27 @@ interface Props {
 export function Header({ btnBack }: Props) {
   return (
     <Flex
+      as="header"
       maxW="1440px"
-      h="100px"
+      h={["50px", "100px"]}
       margin="0 auto"
       align="center"
       justify="center"
       position="relative"
     >
       {btnBack && (
-        <Image
-          src="/icons/arrowLeft.svg"
-          alt="world trip"
-          position="absolute"
-          left="9.5rem"
-          cursor="pointer"
-        />
+        <NextLink href="/" passHref>
+          <Link position="absolute" left={["1.5rem", "2.5rem", "6.5rem", "9.5rem"]}>
+            <Image w={["24px", "32px"]} src="/icons/arrowLeft.svg" alt="world trip" />
+          </Link>
+        </NextLink>
       )}
 
-      <Image src="/logo.svg" alt="world trip" />
+      <Image
+        w={["81px", "184px"]}
+        src="/logo.svg"
+        alt="world trip"
+      />
     </Flex>
   )
 }
